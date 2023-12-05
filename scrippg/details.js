accd4f884f74ccaaa903d151d23d28f122ed419e
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-05 16:52:49 +0400

    uptade

--- scrippg/details.js
+++ scrippg/details.js
@@ -3,7 +3,9 @@ let id=new URLSearchParams(window.location.search).get("id");
 fetch(`http://localhost:3000/robots/${id}`)
 .then(reponse =>reponse.json())
 .then(data=>{
-    document.body.innerHTML+=
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML= 
     `
     <div class="img">
 <img src="${data.image}" alt="" />
@@ -13,4 +15,5 @@ fetch(`http://localhost:3000/robots/${id}`)
 <p>${data.p}</p>
 </div>
     `
+    document.querySelector(".section1").append(card)
 })
