dacc9adf029597503e05fa65aa8ea8f3be75582b
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-05 11:29:10 +0400

    uptaded

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ let nav=document.querySelector("nav");
 let menu=document.querySelector("#click")
 
 window.onscroll=()=>{
-
-    const scrolled = window.scrollY;
-
-    if (scrolled > 50){
+    if (window.scrollY> 50){
         nav.style.background="rgb(133, 133, 198)";
         nav.style.padding="20px 0"
     }
@@ -15,5 +12,37 @@ window.onscroll=()=>{
         nav.style.padding="32px 0"
         nav.style.backgroundColor="transparent"
     }
-
 }
+
+const robotic = [
+    { name: 'The Crab Wheel', price: 'VIEW DETAILS',p:'Who are in extremely love with eco friendly system.', imageSrc: 'p1.png.webp' },
+    { name: 'The Crab Wheel',price: 'VIEW DETAILS',p:'Who are in extremely love with eco friendly system.', imageSrc: 'https://preview.colorlib.com/theme/robotics/img/p2.png' },
+    { name: 'The Crab Wheel',price: 'VIEW DETAILS',p:'Who are in extremely love with eco friendly system.', imageSrc: 'https://preview.colorlib.com/theme/robotics/img/p3.png' },
+    { name: 'The Crab Wheel', price: 'VIEW DETAILS',p:'Who are in extremely love with eco friendly system.', imageSrc: 'https://preview.colorlib.com/theme/robotics/img/p4.png' }
+  ];
+  
+  
+  const bottom = document.querySelector('.bottom1');
+  
+  
+robotic.forEach(robot => {
+  
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    card.innerHTML = `
+        <div class="img">
+          <img src="${robot.imageSrc}" alt="" />
+        </div>
+
+        <div class="text">
+
+
+        <h5>${robot.name}</h5>
+        <p>${robot.p}</p>
+        <button>${robot.price}</button>
+        </div>
+      `;
+  
+    bottom.appendChild(card);
+  });
